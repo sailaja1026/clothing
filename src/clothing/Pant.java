cfe689617a5f311b40555bf37b8ceac78f3dd520
sailaja <sailaja1026@gmail.com>
2021-11-03 14:45:24 -0700

    Revert "Revert "jira stiry number 1223""
    This reverts commit ca2728551bcf1b76ab9fd1d2b3ffc1f6395f6ce5.

---
+++ src/clothing/Pant.java
@@ -0,0 +1,23 @@
+package clothing;
+
+public class Pant extends ClothingDetails {
+	private String colour;
+	private int size;
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
